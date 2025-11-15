4eac076de332bb4d45ac3a0200b084ee1b4fea61
Julianne Tager-Geffner <tagergef@usc.edu>
2025-11-15 04:02:10 +0000

    Implement equalPaths and pass all tests

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,63 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int depth (Node* root);
 
+int depth (Node* root){
+  if(root == NULL){
+    return 0; // empty subtree
+  }
+
+  if (root->left == NULL && root->right == NULL){
+    return 0; // no leafs
+  }
+
+  int leftDepth = 0;
+  int rightDepth =0;
+
+  if(root->left != NULL){
+    leftDepth = depth(root->left);
+  }
+
+  if(root->right != NULL){
+    rightDepth = depth(root->right);
+  }
+
+  if(leftDepth >rightDepth){
+    return leftDepth+1;
+  } else {
+    return rightDepth+1;
+  }
+
+}
 
 bool equalPaths(Node * root)
-{
-    // Add your code below
+{ 
+  // Add your code below
+  if(root == NULL){
+    return true;
+  }
+
+  if(root->left == NULL && root->right == NULL){
+      return true;
+  }
+  
+  if (root->left != NULL && root->right != NULL){
+    int leftDepth = depth(root->left);
+    int rightDepth = depth(root->right);
+    
+    if(leftDepth != rightDepth){
+      return false;
+    }
+
+    return equalPaths(root->left) && equalPaths(root->right);
+  }
+
+  if(root->left != NULL){
+    return equalPaths(root->left);
+  }
+
+  return equalPaths(root->right);
 
 }
 
